5df5710901ad5636e6e81440e45074f1b8edaae3
Victoria Thi Tran <trant48@student.douglascollge.ca>
2022-06-14 12:27:05 -0700

    using map to express data

--- src/App.js
+++ src/App.js
@@ -7,31 +7,47 @@ class App extends Component {
     super();
 
     this.state = {
-      person1: {
-        firstname: "Gam",
-        lastname: "Tran",
-        phone: "778 999 999",
-        mail: "gamhongnd92@gmail.com"
-      },
-      person2: {
-        firstname: "Hoa",
-        lastname: "Tran",
-        phone: "778 000 000",
-        mail: "gamhongnd92@gmail.com"
-      },
-      person3: {
-        firstname: "Nhi",
-        lastname: "Tran",
-        phone: "778 111 1111",
-        mail: "gamhongnd92@gmail.com"
-      },
-      person4: {
-        firstname: "Xam",
-        lastname: "Nguyen",
-        phone: "778 777 777",
-        mail: "gamhongnd92@gmail.com"
-      },
+      people: [
+        {
+         
+            firstname: "Gam",
+            lastname: "Tran",
+            phone: "778 999 999",
+            mail: "gamhongnd92@gmail.com"
+    
+         
+        }, 
 
+        {
+         
+            firstname: "Hoa",
+            lastname: "Tran",
+            phone: "778 000 000",
+            mail: "gamhongnd92@gmail.com"
+          
+        }, 
+
+        {
+         
+            firstname: "Nhi",
+            lastname: "Tran",
+            phone: "778 111 1111",
+            mail: "gamhongnd92@gmail.com"
+          
+        },
+
+        {
+          
+            firstname: "Xam",
+            lastname: "Nguyen",
+            phone: "778 777 777",
+            mail: "gamhongnd92@gmail.com"
+          
+        }
+  
+
+      ]
+      
       
     }
   }
@@ -39,10 +55,11 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-          <h1>{this.state.person1.firstname}</h1>
-          <h1>{this.state.person2.firstname}</h1>
-          <h1>{this.state.person3.firstname}</h1>
-          <h1>{this.state.person4.firstname}</h1>
+         {
+           this.state.people.map((person) => {
+             return <h1>{person.firstname}</h1>
+           })
+         }
       </div>
     );
   }
