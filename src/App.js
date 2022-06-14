ef312a227ae163a377b6664e30257cb961c5605d
Victoria Thi Tran <trant48@student.douglascollge.ca>
2022-06-14 12:07:32 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      person1: {
+        firstname: "Gam",
+        lastname: "Tran",
+        phone: "778 999 999",
+        mail: "gamhongnd92@gmail.com"
+      },
+      person2: {
+        firstname: "Hoa",
+        lastname: "Tran",
+        phone: "778 000 000",
+        mail: "gamhongnd92@gmail.com"
+      },
+      person3: {
+        firstname: "Nhi",
+        lastname: "Tran",
+        phone: "778 111 1111",
+        mail: "gamhongnd92@gmail.com"
+      },
+      person4: {
+        firstname: "Xam",
+        lastname: "Nguyen",
+        phone: "778 777 777",
+        mail: "gamhongnd92@gmail.com"
+      },
+
+      
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+          <h1>{this.state.person1.firstname}</h1>
+          <h1>{this.state.person2.firstname}</h1>
+          <h1>{this.state.person3.firstname}</h1>
+          <h1>{this.state.person4.firstname}</h1>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
